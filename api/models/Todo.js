4bad5704c996b7b312694adb3efebd79a853d7c5
erenyildirimmm <erenyldrmmm06@gmail.com>
2023-04-06 21:31:17 +0300

    first commit

---
+++ api/models/Todo.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const TodoSchema = new Schema({
+    text: {
+        type: String,
+        required: true
+    },
+    complete: {
+        type: Boolean,
+        default: false
+    },
+    timestamp: {
+        type: String,
+        default: new Date().toLocaleDateString()
+    }
+})
+
+const Todo = mongoose.model("Todo", TodoSchema);
+
+module.exports = Todo;
